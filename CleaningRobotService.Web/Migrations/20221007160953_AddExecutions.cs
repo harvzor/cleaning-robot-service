70b4aca67cf226bd78145bba8105a546f8e503aa
harvzor <harveyjwilliams@aol.co.uk>
2022-10-07 18:10:21 +0200

    Migration.

---
+++ CleaningRobotService.Web/Migrations/20221007160953_AddExecutions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CleaningRobotService.Web.Migrations
+{
+    public partial class AddExecutions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "executions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    timestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    commands = table.Column<int>(type: "integer", nullable: false),
+                    result = table.Column<int>(type: "integer", nullable: false),
+                    duration = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_executions", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "executions");
+        }
+    }
+}
