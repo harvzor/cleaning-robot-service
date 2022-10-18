7aa85f97491b39fe76b36d2fc7cd8678c13c11c3
harvzor <harveyjwilliams@aol.co.uk>
2022-10-18 14:30:35 +0200

    Console application.

---
+++ CleaningRobotService.Console/Enums/LetterDirectionEnum.cs
@@ -0,0 +1,9 @@
+namespace CleaningRobotService.Console.Enums;
+
+public enum LetterDirectionEnum
+{
+    N = 0,
+    E = 1,
+    S = 2,
+    W = 3,
+}
--- CleaningRobotService.Console/Program.cs
+++ CleaningRobotService.Console/Program.cs
@@ -1,3 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Drawing;
+using CleaningRobotService.Common.Dtos.Input;
+using CleaningRobotService.Common.Enums;
+using CleaningRobotService.Common.Interfaces;
+using CleaningRobotService.Common.Objects;
+using CleaningRobotService.Console.Enums;
 
-Console.WriteLine("Hello, World!");
+// 2
+// 10 22
+// E 2
+// N 1
+
+int numberOfCommands = int.Parse(Console.ReadLine()!);
+
+// 10 22
+int[] xAndY = Console
+    .ReadLine()!
+    .Split(' ')
+    .Select(int.Parse)
+    .ToArray();
+
+Point startPoint = new Point(x: xAndY[0], y: xAndY[1]);
+List<CommandDto> commands = new();
+
+for (int i = 0; i < numberOfCommands; i++)
+{
+    // E 2
+    // N 1
+    string[] directionAndSteps = Console
+        .ReadLine()!
+        .Split(' ')
+        .ToArray();
+    
+    commands.Add(new CommandDto
+    {
+        // TODO: write a test to ensure LetterDirectionEnum is mapped correctly to DirectionEnum.
+        Direction = (DirectionEnum)Enum.Parse<LetterDirectionEnum>(directionAndSteps[0]),
+        Steps = uint.Parse(directionAndSteps[1])
+    });
+}
+
+// IRobot robot = new RobotPoints
+// IRobot robot = new RobotLines
+IRobot robot = new RobotGrid
+// IRobot robot = new RobotSwarm
+{
+    StartPoint = startPoint,
+    Commands = commands,
+};
+
+robot.CalculatePointsVisited();
+int uniqueStepsCounted = robot.CountPointsVisited();
+
+Console.WriteLine("Cleaned: " + uniqueStepsCounted);
