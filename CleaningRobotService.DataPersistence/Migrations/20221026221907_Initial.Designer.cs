fce9d0fb5555a5fb2a74c82de6dfbeaca0ce301e
harvzor <harveyjwilliams@aol.co.uk>
2022-10-27 00:30:11 +0200

    New migrations.

--- CleaningRobotService.DataPersistence/Migrations/20221026221907_Initial.Designer.cs
+++ CleaningRobotService.DataPersistence/Migrations/20221026221907_Initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CleaningRobotService.DataPersistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CleaningRobotService.DataPersistence.Migrations
 {
     [DbContext(typeof(ServiceDbContext))]
-    partial class ServiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221026221907_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CleaningRobotService.DataPersistence/Migrations/20221026221907_Initial.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleaningRobotService.DataPersistence.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "command_robots",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    start_point_x = table.Column<int>(type: "integer", nullable: false),
+                    start_point_y = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    modified_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    deleted_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_command_robots", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "command_robots_commands",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    direction = table.Column<string>(type: "text", nullable: false),
+                    steps = table.Column<long>(type: "bigint", nullable: false),
+                    command_robot_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    modified_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    deleted_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_command_robots_commands", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_command_robots_commands_command_robots_command_robot_id",
+                        column: x => x.command_robot_id,
+                        principalTable: "command_robots",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "executions",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    commands = table.Column<int>(type: "integer", nullable: false),
+                    result = table.Column<int>(type: "integer", nullable: true),
+                    duration = table.Column<TimeSpan>(type: "interval", nullable: true),
+                    command_robot_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    modified_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    deleted_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_executions", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_executions_command_robots_command_robot_id",
+                        column: x => x.command_robot_id,
+                        principalTable: "command_robots",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_command_robots_commands_command_robot_id",
+                table: "command_robots_commands",
+                column: "command_robot_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_executions_command_robot_id",
+                table: "executions",
+                column: "command_robot_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "command_robots_commands");
+
+            migrationBuilder.DropTable(
+                name: "executions");
+
+            migrationBuilder.DropTable(
+                name: "command_robots");
+        }
+    }
+}
